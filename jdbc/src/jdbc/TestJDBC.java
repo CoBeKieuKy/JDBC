f082d6bd24c65b2b9cd4252dc54c677fb5aed0fc
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2019-07-17 16:09:12 +0700

    update

--- jdbc/src/jdbc/TestJDBC.java
+++ jdbc/src/jdbc/TestJDBC.java
@@ -3,6 +3,10 @@ package jdbc;
 import java.sql.*;
 
 class TestJDBC {
+	static String dbname = "admin2?userSSL=false";
+	static String username = "root";
+	static String password = "admin";
+	
 	public static void main(String args[]) throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
 		printTable();
@@ -11,7 +15,7 @@ class TestJDBC {
 	public static void printTable() {
 		try {
 			System.out.println("The data in the table");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/admin2?"+"useSSL=false", "root", "admin");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbname, username, password);
 			CallableStatement getMember = con.prepareCall("{call getMember()}");
 			ResultSet rs = getMember.executeQuery();
 			while (rs.next())
